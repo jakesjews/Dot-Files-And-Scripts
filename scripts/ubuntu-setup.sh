109a63a6b59186cd9b0d5abc6d4b595e7d8ae220
Jacob Jewell <jakesjews@gmail.com>
2014-04-24 22:03:38 -0400

    Update ubuntu-setup.sh

--- scripts/ubuntu-setup.sh
+++ scripts/ubuntu-setup.sh
@@ -4,30 +4,28 @@ sudo apt-get dist-upgrade -y
 
 # add ppas
 sudo add-apt-repository -y ppa:webupd8team/java
-sudo add-apt-repository -y ppa:webupd8team/sublime-text-2
-sudo add-apt-repository -y ppa:juju/devel
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo add-apt-repository -y ppa:rquillo/ansible
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
 sudo sh -c 'echo "deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen" > /etc/apt/sources.list.d/10gen.list'
 
-sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
+sudo sh -c "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9"
 sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 
 sudo apt-get update
 
 sudo apt-get install -y build-essential llvm clang phantomjs \
-  mono-gmcs python2.7 python3.3 ansible tortoisehg \
-  perl ghc cabal-install golang-go golang-go libtool \
-  golang-tools erlang gprolog quickly quickly-ubuntu-template \
-  redis-server oracle-java7-installer nginx gnome-sharp2 \
-  zookeeper zookeeperd zookeeper-bin sublime-text juju-core \
-  nodejs mongodb-10gen rabbitmq-server tmux vim p7zip \
+  python2.7 ansible tortoisehg \
+  perl ghc cabal-install golang-go libtool \
+  erlang gprolog \
+  redis-server oracle-java8-installer nginx \
+  zookeeper zookeeperd zookeeper-bin \
+  nodejs mongodb-org tmux vim p7zip \
   cmake wget curl exuberant-ctags htop zsh subversion \
-  mercurial git cvs unzip automake pv dtrx rsync libglade2.0-cil-dev \
-  parallel ack cifs-utils smbclient winbind vsftpd \
-  gawk ack-grep ufw libnss-mdns sshpass apt-offline \
+  mercurial git cvs unzip automake pv dtrx rsync \
+  parallel cifs-utils smbclient winbind \
+  gawk ufw libnss-mdns sshpass apt-offline \
   python-httplib2 linux-firmware-nonfree python-keyczar \
   libboost-all-dev scons lxc-docker
 
@@ -37,15 +35,15 @@ sudo dpkg -i bcompare-3.3.8.16340_amd64.deb
 sudo apt-get -f -y install
 
 # robomongo
-wget http://robomongo.org/files/linux/robomongo-0.8.3-x86_64.deb
-sudo dpkg -i robomongo-0.8.3-x86_64.deb
+wget http://robomongo.org/files/linux/robomongo-0.8.4-x86_64.deb
+sudo dpkg -i robomongo-0.8.4-x86_64.deb
 sudo apt-get -f -y install
 
 # install rvm and ruby
 curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enabled
 
 # npm packages
-sudo npm install -g mocha cucumber coffee-script iced-coffee-script brunch bower mocha-phantomjs
+sudo npm install -g mocha coffee-script iced-coffee-script brunch bower mocha-phantomjs
 
 # clojure
 wget https://raw.github.com/technomancy/leiningen/stable/bin/lein
@@ -65,50 +63,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i ./google-chrome*.deb
 sudo apt-get -f -y install
 
-mkdir ~/apps
-
-# intellij
-wget http://download.jetbrains.com/idea/ideaIU-12.1.6.tar.gz
-tar xfz ideaIU-12.1.6.tar.gz
-mv idea-IU-129.713 ~/apps/idea
-
-# rubymine
-wget http://download.jetbrains.com/ruby/RubyMine-5.4.3.tar.gz -T 5
-tar xfz RubyMine-5.4.3.tar.gz
-mv RubyMine-5.4.3 ~/apps/rubymine
-
-# juju keygen
-ssh-keygen -t rsa -b 2048
-juju generate-config
-
-rm *.deb
-rm *.gz
-
-# mono
-mkdir -p dev/src
-cd dev/src
-git clone https://github.com/mono/mono.git --depth 100
-cd mono
-git checkout 'mono-3.2.3'
-git submodule update --init --recursive
-./autogen.sh --prefix=/usr
-make -j2
-sudo make install
-make clean
-
-cd ~/dev/src
-git clone https://github.com/mono/xsp.git --depth 100
-cd xsp
-./autogen.sh --prefix=/usr
-make -j2
-sudo make install
-make clean
-
-cd ~/dev/src
-git clone https://github.com/mono/monodevelop.git
-cd monodevelop
-git submodule update --init --recursive
-./configure --profile=dist
-make -j2
-sudo make install
 make clean
