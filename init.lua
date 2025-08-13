354087f046e9ab3947974e7bca7f3d808b0317e5
Jacob Jewell <jacobjewell@bypass.dev>
2025-08-13 19:01:45 -0400

    sync

--- init.lua
+++ init.lua
@@ -108,18 +108,16 @@ vim.api.nvim_create_autocmd('CursorHold', {
   end
 })
 
-local jsInlayHints = {
-  parameterNames = { enabled = 'literals' },
-  parameterTypes = { enabled = true },
-  variableTypes = { enabled = true },
-  propertyDeclarationTypes = { enabled = true },
-  functionLikeReturnTypes = { enabled = true },
-  enumMemberValues = { enabled = true },
-}
-
 local jsOptions = {
   preferGoToSourceDefinition = true,
-  -- inlayHints = jsInlayHints,
+  -- inlayHints = {
+  --   parameterNames = { enabled = 'literals' },
+  --   parameterTypes = { enabled = true },
+  --   variableTypes = { enabled = true },
+  --   propertyDeclarationTypes = { enabled = true },
+  --   functionLikeReturnTypes = { enabled = true },
+  --   enumMemberValues = { enabled = true },
+  -- },
   suggest = {
     completeFunctionCalls = true,
   },
